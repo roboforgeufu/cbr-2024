c2cef18256f6ae43a777f9797fd331ac2f132401
me15degrees <me15degrees@gmail.com>
2024-10-30 13:09:22 -0300

    feat: add functions prototype

--- src/domain/boarding.py
+++ src/domain/boarding.py
@@ -1,17 +1,23 @@
 from core.robot import Robot
 import constants as const
+from pybricks.parameters import Color
 
+def smart_walk(robot:Robot):
+    """O que será a movimentação no início e 
+    envia mensagens de abrir e fechar a garra"""
 
-def passenger_boarding(robot: Robot):
-    """
-    Rotina de encontrar um passageiro, pega-lo e detectar faixa etária e cor.
+def passenger_read_color_and_type(robot:Robot):
+   """ Retorna cor e se é adulto ou criança -> ("CHILD", Color.BLUE)
+        """
+def passenger_read_type(robot:Robot):
+    """Retorna se é adulto ou criança"""
 
-    Retorna uma tupla como ("CHILD", Color.BLUE) ou ("ADULT", Color.GREEN)
-    """
-    ...
-
+def passenger_traject(robot:Robot):
+    """Chama funções de movimentação depois 
+    de ver quais vértices são o início e o fim"""
 
 def passenger_unboarding(robot: Robot):
+
     """
     Rotina de desembarque de passageiro
     """
