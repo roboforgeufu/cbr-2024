c86073a161216fc5efeb4d1e0c5afbcc78354530
Eric Martins Vilela <ericmvilela@hotmail.com>
2024-11-14 10:21:05 -0300

    feat: new unboarding

--- src/domain/boarding.py
+++ src/domain/boarding.py
@@ -70,20 +70,24 @@ def passenger_unboarding(robot: Robot):
     
     robot.align(speed=35)
     robot.pid_walk(1.5, 30)
-    while robot.color_right.color() != Color.YELLOW or robot.color_left.color() != Color.YELLOW:
-        left_color = robot.color_left.color()
-        right_color = robot.color_right.color()
-
-        robot.pid_walk(12, -30)
-        
-        if left_color == Color.YELLOW:
-            robot.pid_turn(-30)
-        elif right_color == Color.YELLOW:
-            robot.pid_turn(30)
+    
+    if robot.color_right.color() == Color.YELLOW:
+        while robot.color_left.color() != Color.YELLOW:
+            robot.pid_walk(12, -30)
+            robot.pid_turn(-30)    
             
-        robot.pid_walk(7)
-        robot.align(speed=35)
-        robot.pid_walk(1.5, 30)
+            robot.pid_walk(7)
+            robot.align(speed=40)
+            robot.pid_walk(1.5, 35)    
+    
+    elif robot.color_left.color() == Color.YELLOW:
+        while robot.color_right.color() != Color.YELLOW:
+            robot.pid_walk(12, -30)
+            robot.pid_turn(30)    
+            
+            robot.pid_walk(7)
+            robot.align(speed=40)
+            robot.pid_walk(1.5, 35)
     
     robot.pid_walk(15, -35)
     robot.ev3.speaker.beep()
