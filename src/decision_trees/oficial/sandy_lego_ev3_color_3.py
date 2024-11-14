f0eb8152102ca22f6bacf9dc763531c6d181cd1d
Otávio Leite <atoivat.o@gmail.com>
2024-11-14 10:20:32 -0300

    calibração de cor oficial quinta de manha

--- src/decision_trees/oficial/sandy_lego_ev3_color_3.py
+++ src/decision_trees/oficial/sandy_lego_ev3_color_3.py
@@ -1,25 +1,25 @@
 from pybricks.parameters import Color
  # Arquivo gerado automaticamente
 def sandy_lego_ev3_color_p3_decision_tree(R, G, B):
-    if R <= 1.0:
+    if R <= 0.5:
         return None
-    else:  # if R > 1.0
-        if G <= 8.5:
-            if R <= 7.5:
-                if B <= 5.0:
+    else:  # if R > 0.5
+        if B <= 7.0:
+            if R <= 6.5:
+                if G <= 5.5:
                     return Color.BLACK
-                else:  # if B > 5.0
-                    return Color.BLUE
-            else:  # if R > 7.5
-                if R <= 22.0:
-                    return Color.BROWN
-                else:  # if R > 22.0
-                    return Color.RED
-        else:  # if G > 8.5
-            if B <= 22.0:
-                if G <= 22.0:
+                else:  # if G > 5.5
                     return Color.GREEN
-                else:  # if G > 22.0
+            else:  # if R > 6.5
+                if B <= 0.5:
+                    if R <= 18.5:
+                        return Color.BROWN
+                    else:  # if R > 18.5
+                        return Color.RED
+                else:  # if B > 0.5
                     return Color.YELLOW
-            else:  # if B > 22.0
+        else:  # if B > 7.0
+            if B <= 17.5:
+                return Color.BLUE
+            else:  # if B > 17.5
                 return Color.WHITE
