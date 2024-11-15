48f87db47b62b954308f5661103ad0598afb16b4
Otávio Leite <atoivat.o@gmail.com>
2024-11-15 09:48:37 -0300

    feat: calibração cores garra junior

--- src/decision_trees/oficial/junior_lego_ev3_color_2.py
+++ src/decision_trees/oficial/junior_lego_ev3_color_2.py
@@ -1,19 +1,19 @@
 from pybricks.parameters import Color
  # Arquivo gerado automaticamente
 def junior_lego_ev3_color_p2_decision_tree(R, G, B):
-    if R <= 0.5:
+    if R <= 1.0:
         return None
-    else:  # if R > 0.5
-        if B <= 20.0:
-            if G <= 11.5:
-                if R <= 15.5:
+    else:  # if R > 1.0
+        if R <= 7.0:
+            return Color.GREEN
+        else:  # if R > 7.0
+            if B <= 21.5:
+                if R <= 15.0:
                     return Color.BROWN
-                else:  # if R > 15.5
+                else:  # if R > 15.0
                     return Color.RED
-            else:  # if G > 11.5
-                return Color.GREEN
-        else:  # if B > 20.0
-            if R <= 22.5:
-                return Color.BLUE
-            else:  # if R > 22.5
-                return Color.WHITE
+            else:  # if B > 21.5
+                if R <= 20.0:
+                    return Color.BLUE
+                else:  # if R > 20.0
+                    return Color.WHITE
